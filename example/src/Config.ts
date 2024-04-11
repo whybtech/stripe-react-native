896b4e93881679e98b55516ad5623a047c455f85
Yann Bergès <yann.berges@gmail.com>
2024-04-11 16:35:17 +0200

    remove example folder

--- example/src/Config.ts
+++
@@ -1,9 +0,0 @@
-import { Platform } from 'react-native';
-
-// Address to stripe server running on local machine
-const LOCAL_URL =
-  Platform.OS === 'android' ? 'http://10.0.2.2:4242' : 'http://localhost:4242';
-// Address to stripe server running on Glitch
-const GLITCH_URL = 'https://rigorous-heartbreaking-cephalopod.glitch.me';
-
-export const API_URL = __DEV__ ? LOCAL_URL : GLITCH_URL;
